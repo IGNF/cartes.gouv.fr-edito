b20a1d45d467374089ca98f4c34f24d29e7d33c1
Manuel Frangi <manuel.frangi@ign.fr>
2026-01-13 09:38:12 +0100

    refonte de la page /decouvrir

---
+++ public/js/accueil.js
@@ -0,0 +1,21 @@
+let setCardsView = function() {
+    let secondRowCards = document.getElementsByName("second-row-cards");
+
+    for(let i = 0; i < secondRowCards.length; i++) {
+        secondRowCards[i].classList.toggle("hidden");
+    }
+
+    let btn = document.getElementById("fr-button-:r29:");
+
+    if(btn.classList.contains("fr-icon-add-line")) {
+        btn.classList.remove("fr-icon-add-line");
+        btn.classList.add("fr-icon-subtract-line");
+
+        btn.innerText = "Afficher moins";
+    } else {
+        btn.classList.remove("fr-icon-subtract-line");
+        btn.classList.add("fr-icon-add-line");
+
+        btn.innerText = "Afficher plus";
+    }
+};
