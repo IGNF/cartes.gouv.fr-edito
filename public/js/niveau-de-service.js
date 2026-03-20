9b384985e620f79156ade945b82795e00a938dab
Sylvain Lafay <sylvain.lafay@ign.fr>
2026-03-20 09:06:37 +0100

    fix: severity et details des alertes

--- public/js/niveau-de-service.js
+++ public/js/niveau-de-service.js
@@ -1,4 +1,6 @@
 //Récupération des alertes
+const md = window.markdownit();
+
 fetch("https://data.geopf.fr/annexes/cartes.gouv.fr-config/public/alerts.json", { method: "GET" }).then(async function (res) {
 //fetch("https://gpf-annex-qua.priv.geopf.fr/annexes/cartes.gouv.fr-config/public/alerts.json", { method: "GET" }).then(async function (res) {
     let result = await res.json();
@@ -20,14 +22,30 @@ let stopLoader = function() {
 
 let createAlertHTML = function(alert) {
     let div = document.createElement("div");
+
     div.classList.add("fr-alert");
-    div.classList.add("fr-alert--" + alert.severity);
+
+    // severity peut valoir "info" | "warning" | "alert"
+    // les classes DSFR sont "fr-alert--info" | "fr-alert--warning" | "fr-alert--error"
+    let severity = alert.severity === "alert" ? "error": alert.severity;
+    div.classList.add("fr-alert--" + severity);
+
     let titre = document.createElement("h3");
     titre.classList.add("fr-alert__title");
     titre.innerText = alert.title?alert.title:"";
-    let desc = document.createElement("p");
-    desc.innerText = alert.description?alert.description:"";
     div.append(titre);
-    div.append(desc);
+
+    if (alert.description) {
+        let desc = document.createElement("p");
+        desc.innerText = alert.description;
+        div.append(desc);
+    }
+
+    if (alert.details) {
+        let details = document.createElement("div");
+        details.innerHTML = md.render(alert.details);
+        div.append(details);
+    }
+
     document.getElementById("service-alert").append(div);
 };
