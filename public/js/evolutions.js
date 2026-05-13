e1ec90264cdd576133a7f9072a812c96504135cc
MFrangi <45257509+MFrangi@users.noreply.github.com>
2026-05-13 09:19:24 +0200

    feat: création de la page 'evolutions' #49 (#55)
    * feat: création de la page 'evolutions' #49
    
    * fix: suppression du badge 'général' et ajout du badge 'contribuer'
    
    * fix : corrections couleurs
    
    * fix: gestion du theme sombre et corrections diverses
    
    * fix : correction coquilles et erreurs de copier/coller
    
    * fix: apostrophe typographique
    
    ---------
    
    Co-authored-by: Clement Brousseau <clement.brousseau@ign.fr>

---
+++ public/js/evolutions.js
@@ -0,0 +1,24 @@
+let setTabView = function(elem) {
+    
+    let tabs = document.querySelectorAll(".trimester_tab p");
+
+    for(let i in tabs) {
+        if(tabs[i].classList.contains("selected")) {
+            tabs[i].classList.remove("selected");
+            break;
+        }
+    }
+
+    elem.classList.add("selected");
+
+    let tabsContent = document.querySelectorAll(".tab_content");
+
+    for(let i in tabsContent) {
+        if(tabsContent[i].classList.contains("selected")) {
+            tabsContent[i].classList.remove("selected");
+            break;
+        }
+    }
+
+    document.getElementById(elem.dataset.tab_id).classList.add("selected");
+};
