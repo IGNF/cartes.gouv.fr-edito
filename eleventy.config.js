eaadc1cacb10695387ddf4dc61df8ba64febd371
Orka Arnest CRUZE <orka-arnest.cruze@ign.fr>
2026-06-23 17:56:37 +0200

    fix: suppr imports inutilisés

--- eleventy.config.js
+++ eleventy.config.js
@@ -20,9 +20,6 @@ const customMarkdownContainers = require("./markdown-custom-containers");
 
 const { getSummaryItems } = require("./utils.js");
 
-const fs = require("node:fs/promises");
-const path = require("node:path");
-
 module.exports = function (eleventyConfig) {
     // Copy the contents of the `public` folder to the output folder
     // For example, `./public/css/` ends up in `_site/css/`
