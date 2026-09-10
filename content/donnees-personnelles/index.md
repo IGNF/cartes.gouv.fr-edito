ea6dc1b7eee4f0ace642addd0c2629c502a25cc6
Manuel Frangi <manuel.frangi@ign.fr>
2026-09-10 16:53:44 +0200

    feat : regrouper l'opt-out d'Eulerian et de Matomo dans une seule checkbox

--- content/donnees-personnelles/index.md
+++ content/donnees-personnelles/index.md
@@ -227,25 +227,9 @@ Les cookies de mesure d’audience déposés sont les suivants :
 
 Bien que ces cookies Matomo et Eulerian soient exemptés de consentement, vous disposez d'un droit d'opposition total à leur utilisation. Si vous choisissez de vous opposer, un cookie technique anonyme sera déposé sur votre navigateur pour mémoriser votre choix de manière à ce qu'aucune donnée de navigation ne soit collectée lors de vos prochaines visites.
 
-**S'opposer au suivi Matomo :**
-
-Veuillez utiliser le module ci-dessous pour activer votre option de refus :
-
-<div id="matomo-opt-out">
-</div>
-<script src="https://matomo.ign.fr/?module=CoreAdminHome&action=optOutJS&language=auto&div=matomo-opt-out">
-</script>
-
-</br>
-
-**S'opposer au suivi Eulerian :**
-
-Veuillez utiliser le module ci-dessous pour activer votre option de refus :
-
-<p>Vous pouvez vous opposer au suivi de votre navigation sur ce site web. Cela protégera votre vie privée, mais empêchera également le propriétaire d'apprendre de vos actions et de créer une meilleure expérience pour vous et les autres utilisateurs.</p>
 <div class="fr-checkbox-group">
-  <input id="trackVisits-eulerian" type="checkbox" aria-describedby="checkbox-messages">
-  <label class="fr-label" for="trackVisits-eulerian"><strong><span>Vous n'êtes pas exclu(e). Décochez cette case pour vous exclure.</span></strong></label>
+  <input id="trackVisits" type="checkbox" aria-describedby="checkbox-messages">
+  <label class="fr-label" for="trackVisits"><strong><span>Vous n'êtes pas exclu(e). Décochez cette case pour vous exclure.</span></strong></label>
   <div class="fr-messages-group" id="checkbox-messages" aria-live="polite">
 </div>
 
--- public/js/consent-manager.js
+++ public/js/consent-manager.js
@@ -1,15 +1,42 @@
-//initialise le formulaire d'opt-out Eulerian
+/*
+ * Fonction d'initialisation de la checkbox d'opt-out
+ */
 
-if(document.getElementById("trackVisits-eulerian")) {
-  document.documentElement.addEventListener("dsfr.start", () => {
-    if(window.dsfr.analytics.opt.isDisabled) {
-    document.getElementById("trackVisits-eulerian").checked = false;
-    } else {
-      document.getElementById("trackVisits-eulerian").checked = true;
-    }
+let setConsentCheckbox = function() {
+  
+  if(!window.dsfr.analytics.opt || !Matomo) { //Si il y a un problème de chargement des tracker, on ne fait rien
+    return;
+  }
+  if(window.dsfr.analytics.opt.isDisabled && !Matomo.getTracker().hasConsent()) {
+      document.getElementById("trackVisits").checked = false;
+      } else {
+        document.getElementById("trackVisits").checked = true;
+      }
+};
 
-    document.getElementById("trackVisits-eulerian").addEventListener("change", ()=>{ window.dsfr.analytics.opt.toggle(); });
-  });
+//Gestion du formulaire d'opt-out Eulerian/Matomo
+if(document.getElementById("trackVisits")) {
+  //si eulerian et matomo sont déjà chargés
+  if(window.dsfr.analytics.opt && Matomo) {
+    setConsentCheckbox();
+  } 
+  //sinon, on attend l'event signalant la mise en route d'eulerian (normalement Matomo se charge plus rapidement)
+  else {
+    document.documentElement.addEventListener("dsfr.start", () => {
+      setConsentCheckbox();
+    });
+  }
+
+  //listener "on change" de la checkbox
+  document.getElementById("trackVisits").addEventListener("change", ()=>{ 
+      if(document.getElementById("trackVisits").checked == false) {
+        window.dsfr.analytics.opt.disable();
+        disableMatomoTracking();
+      } else {
+        window.dsfr.analytics.opt.enable();
+        enableMatomoTracking();
+      }
+     });
 }
 
 
