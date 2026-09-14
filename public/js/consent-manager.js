4734de23fab6c6986ca8fc074533bd4d186f3a7c
Sylvain Lafay <sylvain.lafay@ign.fr>
2026-09-14 11:48:13 +0200

    fix: ajout event pour matomo

--- public/js/consent-manager.js
+++ public/js/consent-manager.js
@@ -2,41 +2,60 @@
  * Fonction d'initialisation de la checkbox d'opt-out
  */
 
-let setConsentCheckbox = function() {
-  
-  if(!window.dsfr.analytics.opt || !Matomo) { //Si il y a un problème de chargement des tracker, on ne fait rien
-    return;
-  }
-  if(window.dsfr.analytics.opt.isDisabled && !Matomo.getTracker().hasConsent()) {
-      document.getElementById("trackVisits").checked = false;
-      } else {
+const isMatomoReady = () => typeof window.Matomo !== "undefined" && typeof window.Matomo.getTracker === "function";
+
+// Désactivation du suivi Matomo
+function disableMatomoTracking() {
+    var _paq = (window._paq = window._paq || []);
+    // 13 mois ≈ 396 jours (durée recommandée par la CNIL) = 396 × 24 = 9504 heures
+    _paq.push(["optUserOut", 9504]);
+}
+
+// Réactivation du suivi Matomo
+function enableMatomoTracking() {
+    var _paq = (window._paq = window._paq || []);
+    _paq.push(["forgetUserOptOut"]);
+}
+
+let setConsentCheckbox = function () {
+    if (!window.dsfr.analytics.opt || !isMatomoReady()) {
+        return;
+    }
+
+    const tracker = window.Matomo.getTracker();
+    if (!tracker || typeof tracker.hasConsent !== "function") {
+        return;
+    }
+
+    if (window.dsfr.analytics.opt.isDisabled && !tracker.hasConsent()) {
+        document.getElementById("trackVisits").checked = false;
+    } else {
         document.getElementById("trackVisits").checked = true;
-      }
+    }
 };
 
 //Gestion du formulaire d'opt-out Eulerian/Matomo
-if(document.getElementById("trackVisits")) {
-  //si eulerian et matomo sont déjà chargés
-  if(window.dsfr.analytics.opt && Matomo) {
-    setConsentCheckbox();
-  } 
-  //sinon, on attend l'event signalant la mise en route d'eulerian (normalement Matomo se charge plus rapidement)
-  else {
-    document.documentElement.addEventListener("dsfr.start", () => {
-      setConsentCheckbox();
+if (document.getElementById("trackVisits")) {
+    //si eulerian et matomo sont déjà chargés
+    if (window.dsfr.analytics.opt && isMatomoReady()) {
+        setConsentCheckbox();
+    }
+    //sinon, on attend que les deux services soient initialisés
+    else {
+        document.documentElement.addEventListener("dsfr.start", () => {
+            setConsentCheckbox();
+        });
+        window.addEventListener("matomo.ready", setConsentCheckbox);
+    }
+
+    //listener "on change" de la checkbox
+    document.getElementById("trackVisits").addEventListener("change", () => {
+        if (document.getElementById("trackVisits").checked == false) {
+            window.dsfr.analytics.opt.disable();
+            disableMatomoTracking();
+        } else {
+            window.dsfr.analytics.opt.enable();
+            enableMatomoTracking();
+        }
     });
-  }
-
-  //listener "on change" de la checkbox
-  document.getElementById("trackVisits").addEventListener("change", ()=>{ 
-      if(document.getElementById("trackVisits").checked == false) {
-        window.dsfr.analytics.opt.disable();
-        disableMatomoTracking();
-      } else {
-        window.dsfr.analytics.opt.enable();
-        enableMatomoTracking();
-      }
-     });
 }
-
-
